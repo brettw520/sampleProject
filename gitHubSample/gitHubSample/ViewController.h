e8e0206b3d1f342b13bdb612bffa2dd6a214eabf
brettw520 <brettw520@gmail.com>
2014-10-17 18:52:17 -0400

    new Sample Project
    blank Project xCode

---
+++ gitHubSample/gitHubSample/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  gitHubSample
+//
+//  Created by Brett Walfish on 10/17/14.
+//  Copyright (c) 2014 Brett Walfish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
