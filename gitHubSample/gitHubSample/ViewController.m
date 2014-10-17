3cfa056e1c5f571994d177b0a6d9d1de19debffb
brettw520 <brettw520@gmail.com>
2014-10-17 19:21:28 -0400

    Hello World Label added
    For unknown reasons will not center: but it is added

--- gitHubSample/gitHubSample/ViewController.m
+++ gitHubSample/gitHubSample/ViewController.m
@@ -18,6 +18,9 @@
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
     
+    self.myLabel.text = @"Hello World";
+    self.myLabel.textAlignment =  NSTextAlignmentCenter;
+    
     NSLog(@"GitHub update test");
 }
 
